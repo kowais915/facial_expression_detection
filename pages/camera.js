bc0e8bae45475422d291a31d7eb518a19576b6e5
kowais915 <kowais915@gmail.com>
2023-06-11 18:57:44 +0500

    :sparkles: Got the faceapi to detect expressions and faces

--- pages/camera.js
+++ pages/camera.js
@@ -2,17 +2,54 @@ import React, { useState, useRef } from "react";
 import Image from 'next/image';
 import {Camera} from "react-camera-pro";
 import styles from '@/styles/Camera.module.css';
+import { useEffect } from 'react';
+import * as faceapi from 'face-api.js';
 
 const CameraPage = () => {
-
+    const canvasRef = useRef();
+    const imageRef = useRef();
     const camera = useRef(null);
     const [image, setImage] = useState(null);
 
+
+    const webcam = async ()=>{
+        const detect = await faceapi.detectAllFaces(imageRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        console.log(detect)
+        faceapi.matchDimensions(canvasRef.current, {
+          width: "900",
+          height: "600"
+        });
+        canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imageRef.current);
+        faceapi.draw.drawDetections(canvasRef.current, detect);
+      }
+
+      useEffect(()=>{
+        const loadingModels =()=>{
+          Promise.all([
+            faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+            faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+            faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+            faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+          ])
+          .then(()=>{
+            console.log(webcam())
+          })
+          .catch(err=>console.log(err))
+        }
+    
+        imageRef.current && loadingModels();
+      },[])
+
     return ( 
         <div style={{width: "400px"}}>
-            <Camera ref={camera}/>
+
+
+            <Camera ref={imageRef}/>
             <button onClick={() => setImage(camera.current.takePhoto())}>Take photo</button>
             <img src={image} alt='Taken photo'/>
+
+      
+            <canvas ref={canvasRef} width="700" height="600" className={styles.canvas}></canvas>
       </div>
      );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,62 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import {useRef } from 'react';
+import { useEffect } from 'react';
+import * as faceapi from 'face-api.js';
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
+  const canvasRef = useRef();
+  const imageRef = useRef();
+
+  const webcam = async ()=>{
+    const detect = await faceapi.detectAllFaces(imageRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    console.log(detect)
+    faceapi.matchDimensions(canvasRef.current, {
+      width: "900",
+      height: "600"
+    });
+
+    const scaled = faceapi.resizeResults(detect, {width: "900", height: "600"});
+    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imageRef.current);
+    faceapi.draw.drawFaceExpressions(canvasRef.current, scaled);
+    faceapi.draw.drawDetections(canvasRef.current, scaled);
+  }
+
+  useEffect(()=>{
+    const loadingModels =()=>{
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+      ])
+      .then(()=>{
+        console.log(webcam())
+      })
+      .catch(err=>console.log(err))
+    }
+
+    imageRef.current && loadingModels();
+  },[])
+
+
   return (
-    <>
-      <h1>This is the facial expression detection app.</h1>
-    </>
+    <div className={styles.container}>
+      <Image
+        src="/image4.webp"
+        alt="Picture of the author"
+        width={900}
+        height={600}
+        ref={imageRef}
+
+      ></Image>
+    
+      
+      <canvas ref={canvasRef} width="900" height="600" className={styles.canvas}></canvas>
+    </div>
   )
 }
